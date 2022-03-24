fca284782b61d1ed01d0a90484800f678d69e8d2
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2022-03-24 19:09:35 +0200

    fex some issues and fex sum bug

--- server/router/pages/index.js
+++ server/router/pages/index.js
@@ -3,9 +3,9 @@ const { authenticateToken, redirectToDefault } = require('../../middleware');
 const auth = require('./auth.router');
 const profile = require('./profile.router');
 
-const pageRoute = express();
+const pageRoute = express.Router();
 
-pageRoute.use('/', redirectToDefault, auth);
-pageRoute.use('/', authenticateToken, profile);
+pageRoute.use('/auth', redirectToDefault, auth);
+pageRoute.use('/profile', authenticateToken, profile);
 
 module.exports = pageRoute;

774b47c9f9a62402c6f36c9f4bc21581c271f92f
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2022-03-28 21:30:45 +0300

    Create Test Create and Update and Delete Posts and show posts

--- __test__/database.test.js
+++ __test__/database.test.js
@@ -16,6 +16,12 @@ const {
   updateUserProfileQuery,
   getUserPasswordQuery,
   updateUserPasswordQuery,
+  createPostQuery,
+  showPostQuery,
+  deletePostQuery,
+  updatePostQuery,
+  getLastFivePostsQuery,
+  getTopFiveVotedPostsQuery,
 } = require('../server/database/queries');
 const { hashPassword } = require('../server/util');
 
@@ -38,6 +44,15 @@ const [
   hashPassword('test_user_Pa$$w0rd'),
 ];
 const [id, email] = [1, 'ahmed_qeshta@gmail.com'];
+
+const [postId, userId, postTitle, postContent, postUrlImage, postCreatedAt] = [
+  3,
+  1,
+  'First Post test',
+  'First Post content test',
+  'https://oshiprint.in/image/data/poster/new/mqp1193.jpeg',
+  TimeNow,
+];
 
 // run after each test
 beforeEach(() => dbBuilder());
@@ -121,6 +136,79 @@ describe('Test ::  ===>  Table Users <=== :: In DataBase', () => {
 describe('Test ::  ===>  Table Posts <=== :: In DataBase', () => {
   test('Test Get All Posts Should return (Three), we have three post', () =>
     getPostsQuery().then(({ rowCount }) => {
+      expect(rowCount).toBe(3);
+    }));
+
+  test('Test Get Create Post Should return (true)', () =>
+    createPostQuery(
+      userId,
+      postTitle,
+      postContent,
+      postUrlImage,
+      postCreatedAt,
+    ).then(({ rowCount, rows }) => {
+      expect(rowCount).toBe(1);
+      expect(rows[0].id).toBe(4);
+      expect(rows[0].user_id).toBe(userId);
+      expect(rows[0].title).toBe(postTitle);
+      expect(rows[0].content).toBe(postContent);
+      expect(rows[0].url_image).toBe(postUrlImage);
+    }));
+
+  test('Test show Post Should return (true)', () =>
+    showPostQuery(postId).then(({ rowCount }) => {
+      expect(rowCount).toBe(1);
+    }));
+
+  test('Test show Post Should return (false)', () =>
+    showPostQuery(idValid).then(({ rowCount }) => {
+      expect(rowCount).toBe(0);
+    }));
+
+  //
+  test('Test delete Post Should return (true)', () =>
+    deletePostQuery(postId, userId).then(({ rowCount, rows }) => {
+      expect(rowCount).toBe(1);
+      expect(rows[0].id).toBe(3);
+      expect(rows[0].user_id).toBe(1);
+      expect(rows[0].title).toBe('Post 3');
+      expect(rows[0].content).toBe('This is the third post');
+      expect(rows[0].url_image).toBe(
+        'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+      );
+    }));
+
+  test('Test delete Post Should return (false)', () =>
+    deletePostQuery(idValid, userId).then(({ rowCount }) => {
+      expect(rowCount).toBe(0);
+    }));
+
+  test('Test update Post Should return (true), we have three post', () =>
+    updatePostQuery(postId, userId, postTitle, postContent, postUrlImage).then(
+      ({ rowCount, rows }) => {
+        expect(rowCount).toBe(1);
+        expect(rows[0].id).toBe(postId);
+        expect(rows[0].user_id).toBe(userId);
+        expect(rows[0].title).toBe(postTitle);
+        expect(rows[0].content).toBe(postContent);
+        expect(rows[0].url_image).toBe(postUrlImage);
+      },
+    ));
+  test('Test update Post Should return (true), we have three post', () =>
+    updatePostQuery(idValid, userId, postTitle, postContent, postUrlImage).then(
+      ({ rowCount }) => {
+        expect(rowCount).toBe(0);
+      },
+    ));
+
+  test('Test Get get Last Five Posts Should return (true), we have three post', () =>
+    getLastFivePostsQuery().then(({ rowCount }) => {
+      expect(rowCount).toBe(3);
+    }));
+  //
+
+  test('Test get Top Five Voted Posts Should return (true), we have three post', () =>
+    getTopFiveVotedPostsQuery().then(({ rowCount }) => {
       expect(rowCount).toBe(3);
     }));
 });

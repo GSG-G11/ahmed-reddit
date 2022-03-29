4b5b9e1ac7ad7c0c93fc22a96fe12eda6633ace9
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2022-03-29 21:20:33 +0300

    Fix Some deploy

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 const app = require('./app');
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(
-    `Server is Running on http://localhost:${process.env.PORT || 3000}`,
-  );
+app.listen(app.get('port'), () => {
+  console.log(`Server is Running on http://localhost:${app.get('port')}`);
 });
